74e9887022e688b677a36600c4e1eb039e60d810
Abel Cabeza Román <contacto@abelcabezaroman.com>
2022-02-03 16:24:00 +0100

    s

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import './App.css';
-import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
-import { BrowserRouter as Router, Link, Redirect, Route, Switch } from "react-router-dom";
-import PrivateRoute from "./shared/components/PrivateRoute/PrivateRoute";
+import { BrowserRouter, Navigate, NavLink, Route, Routes } from "react-router-dom";
 import AuthButton from "./shared/components/AuthButton/AuthButton";
-import UsersPage from "./pages/UsersPage/HelloUser";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
 import { JwtContext } from './shared/contexts/JwtContext';
+import RequireAuth from "./shared/components/PrivateRoute/RequireAuth";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import UsersPage from "./pages/UsersPage/HelloUser";
 
 function App () {
     const [jwt, setJwt] = useState(localStorage.getItem('token') || null);
@@ -16,33 +16,28 @@ function App () {
         <JwtContext.Provider value={{ jwt, setJwt }}>
             <div className="App">
                 <div className="App-header">
-                    <Router>
+                    <BrowserRouter>
                         <AuthButton/>
 
                         <nav>
-                            {jwt && <Link className="b-btn" to="/hello-user">Profile</Link>}
-                            {jwt && <Link className="b-btn" to="/users">Users</Link>}
-                            <Link className="b-btn" to="/register">Register</Link>
-                            {!jwt && <Link className="b-btn" to="/login">Login</Link>}
+                            {jwt && <NavLink className="b-btn" to="/hello-user">Profile</NavLink>}
+                            {jwt && <NavLink className="b-btn" to="/users">Users</NavLink>}
+                            <NavLink className="b-btn" to="/register">Register</NavLink>
+                            {!jwt && <NavLink className="b-btn" to="/login">Login</NavLink>}
                         </nav>
 
 
-                        <Switch>
-                            <Route path="/register">
-                                <RegisterPage/>
-                            </Route>
-                            <PrivateRoute path="/profile">
-                                <ProfilePage/>
-                            </PrivateRoute>
-                            <PrivateRoute path="/users">
-                                <UsersPage/>
-                            </PrivateRoute>
-                            <Route path="/login">
-                                <LoginPage/>
-                            </Route>
-                            <Redirect from="/" to="login"/>
-                        </Switch>
-                    </Router>
+                        <Routes>
+                            <Route path="/register" element={<RegisterPage/>}/>
+                            <Route path="/profile" element={<RequireAuth><ProfilePage/></RequireAuth>}/>
+                            <Route path="/users" element={<RequireAuth><UsersPage/></RequireAuth>}/>
+                            <Route path="/login" element={<LoginPage/>}/>
+                            <Route
+                                path="/"
+                                element={<Navigate to="/login"/>}
+                            />
+                        </Routes>
+                    </BrowserRouter>
                 </div>
             </div>
         </JwtContext.Provider>
--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -24,15 +24,15 @@ export default function RegisterPage () {
 
             {/* register your input into the hook by invoking the "register" function */}
             <label htmlFor="name">Name</label>
-            <input name="name" id="name" defaultValue="Abel Cabeza Román"
-                   ref={register({ required: true })}/>
+            <input id="name" defaultValue="Abel Cabeza Román"
+                   {...register("name", { required: true })}/>
 
             {/*<input name="role" id="role" defaultValue="admin"*/}
             {/*       ref={register({ required: true })}/>*/}
 
             <label htmlFor="email">Email</label>
-            <input name="email" id="email" defaultValue="contacto@abelcabezaroman.com"
-                   ref={register({ required: true, pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/ })}/>
+            <input id="email" defaultValue="contacto@abelcabezaroman.com"
+                   {...register("email", { required: true, pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/ })}/>
 
             {/*<label htmlFor="username">Username</label>*/}
             {/*<input name="username" id="username" defaultValue="abelcabezaroman"*/}
@@ -41,7 +41,10 @@ export default function RegisterPage () {
             {/* include validation with required or other standard HTML validation rules */}
             <label htmlFor="password">Password</label>
             <input name="password" id="password" type="password" defaultValue="ABCedf123"
-                   ref={register({ required: true, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/ })}/>
+                   {...register("password", {
+                       required: true,
+                       pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
+                   })}/>
             {/* errors will return when field validation fails  */}
             {/*{errors.exampleRequired && <span>This field is required</span>}*/}
 
